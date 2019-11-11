f75a1dfc95aeabe2f5a7d9c6df688cbbde43db8a
Firassov <firas.mrad@esprit.tn>
2019-11-11 19:41:40 +0100

    gestion personnels

---
+++ GestionPersonnels/src/main/java/com/hotel/personnel/GestionPersonnelsApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.personnel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPersonnelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPersonnelsApplication.class, args);
+	}
+
+}
---
+++ GestionPersonnels/src/test/java/com/hotel/personnel/GestionPersonnelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.personnel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPersonnelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
